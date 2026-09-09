1d4cb2a2258f1f8297ac4b5ba7b4719db330853b
Yulia  Demir <Nightelfin91@gmail.com>
2026-09-09 17:24:21 +0300

    chore: lint

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,28 +1,26 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 import perfectionist from 'eslint-plugin-perfectionist';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-    {
-        ignores: ['dist/**', 'node_modules/**'],
+  {
+    ignores: ['dist/**', 'coverage/**'],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  perfectionist.configs['recommended-natural'],
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
-
-    eslint.configs.recommended,
-
-    ...tseslint.configs.recommended,
-
-    perfectionist.configs['recommended-natural'],
-
-    {
-        files: ['**/*.ts'],
-
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-        },
-    },
+  },
 ]);
